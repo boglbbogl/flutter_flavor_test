7f1e8604115ffb4b470d2d3aaa1b0f42a3ec8900
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-06-04 18:00:19 +0900

    firebase setting

--- lib/_src/main_page.dart
+++ lib/_src/main_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flavor_test/_env/enviroment.dart';
 
@@ -7,10 +10,23 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(Enviroment.enviroment.type)),
-      body: Center(
-        child: Text(Enviroment.enviroment.url),
-      ),
-    );
+        appBar: AppBar(title: Text(Enviroment.enviroment.type)),
+        body: SizedBox(
+          width: MediaQueryData.fromWindow(window).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(Enviroment.enviroment.url),
+              GestureDetector(
+                onTap: () async {
+                  UserCredential? _user =
+                      await FirebaseAuth.instance.signInAnonymously();
+                  print(_user.user?.uid);
+                },
+                child: Text("Anonymouse"),
+              )
+            ],
+          ),
+        ));
   }
 }
