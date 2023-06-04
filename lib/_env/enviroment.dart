0775c3280ebab139ffa7d10cc1de2bd969bbcba9
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-06-05 01:20:57 +0900

    Revert "firebase setting"
    This reverts commit 7f1e8604115ffb4b470d2d3aaa1b0f42a3ec8900.

--- lib/_env/enviroment.dart
+++ lib/_env/enviroment.dart
@@ -1,8 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flavor_test/_env/enviroment_type.dart';
 import 'package:flutter_flavor_test/_env/my_app.dart';
-import 'package:flutter_flavor_test/firebase_options.dart';
 
 class Enviroment {
   static late Enviroment _instance;
@@ -16,13 +14,7 @@ class Enviroment {
 
   const Enviroment._internal();
 
-  void run() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    runApp(const MyApp());
-  }
+  void run() => runApp(const MyApp());
 
   static Enviroment get instance => _instance;
   static EnviromentType get enviroment => _type;
