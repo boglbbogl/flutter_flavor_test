be3de0c5656c7b76c30abbe255f750a8922b1d38
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-06-07 10:58:46 +0900

    flavor naver login

--- lib/_src/main_page.dart
+++ lib/_src/main_page.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_flavor_test/_env/enviroment.dart';
+import 'package:flutter_naver_login/flutter_naver_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class MainPage extends StatelessWidget {
@@ -15,30 +16,10 @@ class MainPage extends StatelessWidget {
       body: GestureDetector(
         onTap: () async {
           HapticFeedback.heavyImpact();
-          FirebaseAuth.instance.signOut();
-          GoogleSignIn _googleSignIn = GoogleSignIn();
-          GoogleSignInAccount? _account = await _googleSignIn.signIn();
-          if (_account != null) {
-            GoogleSignInAuthentication _authentication =
-                await _account.authentication;
-            OAuthCredential _googleCredential = GoogleAuthProvider.credential(
-              idToken: _authentication.idToken,
-              accessToken: _authentication.accessToken,
-            );
-            UserCredential _credential = await FirebaseAuth.instance
-                .signInWithCredential(_googleCredential);
-            print(_credential.user?.displayName);
-            print(_credential.user?.email);
-            FirebaseFirestore _firestore = FirebaseFirestore.instance;
-            await _firestore
-                .collection("${Enviroment.enviroment.type}_flavor_test")
-                .doc(_credential.user?.uid)
-                .set(
-              {
-                "uid": _credential.user?.uid,
-              },
-            );
-          }
+
+          NaverLoginResult _login = await FlutterNaverLogin.logIn();
+          print("accessToken ::: ${_login.accessToken}");
+          print(_login.account);
         },
         child: Center(
           child: Text(Enviroment.enviroment.url),
